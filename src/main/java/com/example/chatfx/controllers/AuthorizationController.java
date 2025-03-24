362d23e553f54474e37e832f6aee6394290ca4c0
Komead <dkapitanov18@gmail.com>
2025-03-24 16:43:50 +0300

    commit

--- src/main/java/com/example/chatfx/controllers/AuthorizationController.java
+++ src/main/java/com/example/chatfx/controllers/AuthorizationController.java
@@ -1,12 +1,16 @@
 package com.example.chatfx.controllers;
 
 import com.example.chatfx.ServerHandler;
+import com.google.gson.Gson;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.util.HashMap;
+
 public class AuthorizationController {
     private ServerHandler serverHandler;
+    private Gson gson = new Gson();
 
     @FXML
     private TextField login_tf;
@@ -19,21 +23,45 @@ public class AuthorizationController {
 
     @FXML
     private void onLoginButtonClick() {
-        if (serverHandler.isConnected()) {
-            serverHandler.sendMessage("/login");
-
-            // Ждём ответ
-            serverHandler.checkMessage();
+        if (!serverHandler.isConnected()) {
+            // Обработать отсутствие подключения
         }
+
+        if (login_tf.getText().isEmpty() || password_tf.getText().isEmpty()) {
+            // Обработать ситуацию с пустыми полями
+        }
+
+        HashMap<String, String> data = new HashMap<>();
+
+        data.put("code", "/login");
+        data.put("username", login_tf.getText());
+        data.put("password", password_tf.getText());
+
+        serverHandler.sendMessage(gson.toJson(data));
+
+        // Нужно обработать ответ
+        serverHandler.checkMessage();
     }
 
     @FXML
     private void onRegisterButtonClick() {
-        if (serverHandler.isConnected()) {
-            serverHandler.sendMessage("/register");
-
-            // Ждём ответ
-            serverHandler.checkMessage();
+        if (!serverHandler.isConnected()) {
+            // Обработать отсутствие подключения
         }
+
+        if (login_tf.getText().isEmpty() || password_tf.getText().isEmpty()) {
+            // Обработать ситуацию с пустыми полями
+        }
+
+        HashMap<String, String> data = new HashMap<>();
+
+        data.put("code", "/register");
+        data.put("username", login_tf.getText());
+        data.put("password", password_tf.getText());
+
+        serverHandler.sendMessage(gson.toJson(data));
+
+        // Нужно обработать ответ
+        serverHandler.checkMessage();
     }
 }
